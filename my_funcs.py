b1f0950623bc03be9e25618a7b8d37653298cc4b
Gabe Dorval <gabedorval589@gmail.com>
2025-05-02 14:56:29 -0700

    Created two functions for user info.

---
+++ my_funcs.py
@@ -0,0 +1,20 @@
+def get_user():
+    user_name = input("What is your name? ")
+    dob = input("What is your date of birth? (dd/mm/yyyy) ")
+    fav_desert  = input("What is your favorite desert? ")
+    return(user_name, dob, fav_desert)
+
+user_info = get_user()
+print(user_info)
+
+def build_dict(user_info ):
+    name, dob, desert = user_info
+    my_dict = {
+        'name': name,
+        'dob': dob,
+        'desert': desert
+    }
+    return my_dict
+
+response = build_dict(user_info)
+print(response)
